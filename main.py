0b4292a7263149adaa39ec74be987e495be9f924
Zetoo0 <zeteny000@gmail.com>
2024-05-06 18:29:54 +0200

    removed unused files

--- main.py
+++
@@ -1,62 +0,0 @@
-let s:cpo_save=&cpo
-set cpo&vim
-cnoremap <silent> <Plug>(TelescopeFuzzyCommandSearch) e "lua require('telescope.builtin').command_history { default_text = [=[" . escape(getcmdline(), '"') . "]=] }"
-inoremap <C-W> u
-inoremap <C-U> u
-nnoremap  <Cmd>nohlsearch|diffupdate|normal! 
-nnoremap  :NvimTreeFindFileToggle
-nnoremap  h :nohlsearch
-xnoremap # y?\V"
-omap <silent> % <Plug>(MatchitOperationForward)
-xmap <silent> % <Plug>(MatchitVisualForward)
-nmap <silent> % <Plug>(MatchitNormalForward)
-nnoremap & :&&
-xnoremap * y/\V"
-nnoremap Y y$
-omap <silent> [% <Plug>(MatchitOperationMultiBackward)
-xmap <silent> [% <Plug>(MatchitVisualMultiBackward)
-nmap <silent> [% <Plug>(MatchitNormalMultiBackward)
-omap <silent> ]% <Plug>(MatchitOperationMultiForward)
-xmap <silent> ]% <Plug>(MatchitVisualMultiForward)
-nmap <silent> ]% <Plug>(MatchitNormalMultiForward)
-xmap a% <Plug>(MatchitVisualTextObject)
-omap <silent> g% <Plug>(MatchitOperationBackward)
-xmap <silent> g% <Plug>(MatchitVisualBackward)
-nmap <silent> g% <Plug>(MatchitNormalBackward)
-snoremap <expr> <BS> ("\<BS>" . (&virtualedit ==# '' && getcurpos()[2] >= col('$') - 1 ? 'a' : 'i'))
-nnoremap <Plug>PlenaryTestFile :lua require('plenary.test_harness').test_file(vim.fn.expand("%:p"))
-xmap <silent> <Plug>(MatchitVisualTextObject) <Plug>(MatchitVisualMultiBackward)o<Plug>(MatchitVisualMultiForward)
-onoremap <silent> <Plug>(MatchitOperationMultiForward) :call matchit#MultiMatch("W",  "o")
-onoremap <silent> <Plug>(MatchitOperationMultiBackward) :call matchit#MultiMatch("bW", "o")
-xnoremap <silent> <Plug>(MatchitVisualMultiForward) :call matchit#MultiMatch("W",  "n")m'gv``
-xnoremap <silent> <Plug>(MatchitVisualMultiBackward) :call matchit#MultiMatch("bW", "n")m'gv``
-nnoremap <silent> <Plug>(MatchitNormalMultiForward) :call matchit#MultiMatch("W",  "n")
-nnoremap <silent> <Plug>(MatchitNormalMultiBackward) :call matchit#MultiMatch("bW", "n")
-onoremap <silent> <Plug>(MatchitOperationBackward) :call matchit#Match_wrapper('',0,'o')
-onoremap <silent> <Plug>(MatchitOperationForward) :call matchit#Match_wrapper('',1,'o')
-xnoremap <silent> <Plug>(MatchitVisualBackward) :call matchit#Match_wrapper('',0,'v')m'gv``
-xnoremap <silent> <Plug>(MatchitVisualForward) :call matchit#Match_wrapper('',1,'v'):if col("''") != col("$") | exe ":normal! m'" | endifgv``
-nnoremap <silent> <Plug>(MatchitNormalBackward) :call matchit#Match_wrapper('',0,'n')
-nnoremap <silent> <Plug>(MatchitNormalForward) :call matchit#Match_wrapper('',1,'n')
-nnoremap <C-N> :NvimTreeFindFileToggle
-nnoremap <C-L> <Cmd>nohlsearch|diffupdate|normal! 
-inoremap  u
-inoremap  u
-let &cpo=s:cpo_save
-unlet s:cpo_save
-set autowrite
-set backspace=2
-set completeopt=menuone,noselect,noinsert
-set expandtab
-set helplang=hu
-set runtimepath=~\\AppData\\Local\\nvim,~\\AppData\\Local\\nvim-data\\site,~\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\packer.nvim,C:\\Program\ Files\\Neovim\\share\\nvim\\runtime,C:\\Program\ Files\\Neovim\\share\\nvim\\runtime\\pack\\dist\\opt\\matchit,C:\\Program\ Files\\Neovim\\lib\\nvim,~\\AppData\\Local\\nvim-data\\site/pack/*/start/*,~\\AppData\\Local\\nvim-data\\site/pack/*/start/*\\after,~\\AppData\\Local\\nvim-data\\site\\after,~\\AppData\\Local\\nvim\\after
-set shiftround
-set shiftwidth=2
-set shortmess=FxnTiclOoft
-set statusline=%#Normal#
-set tabstop=2
-set termguicolors
-set updatetime=300
-set wildignore=*.pyc
-set window=40
-" vim: set ft=vim :
